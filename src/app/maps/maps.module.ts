8c04e32f81c0bc2ee195a735a93d2883e51b899c
jere <jere77lagos@gmail.com>
2024-08-12 16:57:30 +0200

    implement fullscreen map with mapbox

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import  mapboxgl from 'mapbox-gl';
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoiamVyZW1pYXNsciIsImEiOiJjbHpxc2F0YjExaGZhMmtzOHRsam1iaGFsIn0.YeJtziTtaORrMOQeXhxh2w';
+
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { MapsRoutingModule } from './maps-routing.module';
