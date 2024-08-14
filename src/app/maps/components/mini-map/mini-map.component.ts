a71f180862837d6953948e525a4d6e88e14e1f63
jere <jere77lagos@gmail.com>
2024-08-14 12:33:59 +0200

    implement properties page

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+
+  @ViewChild('map') public divMap?: ElementRef
+  @Input() lngLat?: [number, number]
+
+  public map?: Map
+  public currentLngLat: LngLat = new LngLat(-3.6674605533050055, 40.399338398353024)
+
+  ngAfterViewInit(): void {
+    if ( !this.divMap?.nativeElement ) throw "Map Div not found"
+    if ( !this.lngLat ) throw "LngLat can't be null"
+
+    //map
+    this.map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    })
+
+    // marker
+    new Marker()
+      .setLngLat (this.lngLat )
+      .addTo( this.map )
+  }
 
 }
